77e0ada6a070b2f80deff0bf34f8be83a6f48794
Kevin Watters <kevinwatters@gmail.com>
2019-11-18 09:28:12 -0500

    batch script update

--- b.bat
+++ b.bat
@@ -1,17 +1,19 @@
 @echo off
 setlocal
-set PATH=c:\src\fips-deploy\sokol-tools\win64-vstudio-debug;%PATH%
+set PATH=c:\src\fips-deploy\sokol-tools\win64-vstudio-release;%PATH%
 set SHDC=sokol-shdc --slang hlsl5 --input
 set OPT_LEVEL=0
 
 set OUT_DIR=src/shader_meta
 
 preprocess.exe &&^
+echo Compiling shaders... &&^
 %SHDC% gizmos.glsl --output %OUT_DIR%/gizmos.odin && ^
 %SHDC% shadertoy.glsl --output %OUT_DIR%/shadertoy.odin && ^
 %SHDC% sdf_text.glsl --output %OUT_DIR%/sdf_text.odin && ^
 %SHDC% cgltf_sapp.glsl --output %OUT_DIR%/cgltf_sapp.odin && ^
 %SHDC% mrt_sapp.glsl --output %OUT_DIR%/mrt_sapp.odin && ^
+echo Building game... &&^
 odin build src -collection=sokol=../odin-sokol/src -opt=%OPT_LEVEL% -debug -ignore-unknown-attributes
 
 REM -show-timings -keep-temp-files 
